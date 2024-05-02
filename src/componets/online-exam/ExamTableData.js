2a7e46284d1bfdef4a30d793f4cbb70c227c29c9
shaguftanoori <shagufta551739@gmail.com>
2024-05-02 11:31:08 +0530

    create table hdr

--- src/componets/online-exam/ExamTableData.js
+++ src/componets/online-exam/ExamTableData.js
@@ -1,14 +1,13 @@
 import { useState, useRef } from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
-import { FilterMatchMode, FilterOperator } from 'primereact/api';
+import { FilterMatchMode } from 'primereact/api';
 import 'primereact/resources/primereact.css';
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
 import 'primeicons/primeicons.css';
-import { Button } from 'primereact/button';
-import { Tooltip } from 'primereact/tooltip';
 import { saveAsExcelFile } from '../../common/utils';
 import { Link } from 'react-router-dom';
+import { RenderHeader } from './tableHdr';
 
 
 const data = [
@@ -111,7 +110,6 @@ export const ExamTableData = () => {
     };
     const exportExcel = () => {
         import('xlsx').then((xlsx) => {
-            console.log('data---', data)
             const worksheet = xlsx.utils.json_to_sheet(data);
             const workbook = { Sheets: { data: worksheet }, SheetNames: ['sheet1'] };
             const excelBuffer = xlsx.write(workbook, {
@@ -123,74 +121,10 @@ export const ExamTableData = () => {
         });
     };
     
-
-
-    console.log('global filter', globalFilterValue)
-    const renderHeader = () => {
-        return (
-            <div className='row me-2'>
-                <div className='col-md-2'>dsfds</div>
-                <div className='col-md-10'>
-                <div className="dt-action-buttons text-xl-end text-lg-start text-md-end text-start d-flex align-items-center justify-content-end flex-md-row flex-column mb-3 mb-md-0">
-                    <div id="DataTables_Table_0_filter" className="dataTables_filter">
-                        <label>
-                            <input
-                                type="search"
-                                className="form-control"
-                                placeholder="Search.."
-                                aria-controls="DataTables_Table_0"
-                                value={globalFilterValue} onChange={onGlobalFilterChange}
-                            />
-                            
-                        </label>
-                    </div>
-                    <div className="dt-buttons btn-group flex-wrap">
-                        {" "}
-                        <div className="btn-group">
-                            <button
-                                className="btn btn-secondary buttons-collection dropdown-toggle btn-label-secondary mx-3 waves-effect waves-light"
-                                tabIndex={0}
-                                aria-controls="DataTables_Table_0"
-                                type="button"
-                                aria-haspopup="dialog"
-                                aria-expanded="false"
-                            >
-                                <span type="button" icon="pi pi-file-excel" severity="success" rounded data-pr-tooltip="XLS" onClick={exportExcel}>
-                                    <i className="ti ti-screen-share me-1 ti-xs" />
-                                    Export
-                                    
-                                </span>
-                            </button>
-                        </div>{" "}
-                        <Link
-                            className="btn btn-secondary add-new btn-primary waves-effect waves-light"
-                            to={'/admin/addEnquiry'}
-                        >
-                            <span>
-                                <i className="ti ti-plus me-0 me-sm-1 ti-xs" />
-                                <span className="d-none d-sm-inline-block" >Add New Enquiry</span>
-                            </span>
-                        </Link>
-                    </div>
-                </div>
-                </div>
-                {/* <span className="p-input-icon-left">
-                    <i className="pi pi-search" />
-                    <input value={globalFilterValue} onChange={onGlobalFilterChange} placeholder="Keyword Search" />
-                </span>
-                <button type="button" icon="pi pi-file-excel" severity="success" rounded data-pr-tooltip="XLS" onClick={exportExcel}>
-                    export
-                </button> */}
-
-            </div>
-        );
-    };
-    
-   
     return (
         <div>
             <DataTable ref={dt} value={products}   rows={5} rowsPerPageOptions={[5, 10, 25, 50]} filterDisplay="row"
-                header={renderHeader()}
+                header={<RenderHeader globalFilterValue={globalFilterValue} onGlobalFilterChange={onGlobalFilterChange} exportExcel={exportExcel}/>}
                 filters={filters}
                 globalFilterFields={['branch']}
             >
